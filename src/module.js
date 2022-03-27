f63c1f8236940ad296710bd3c3a5af476f019b94
Andrey Parshin <jobandrux@yandex.ru>
2022-03-27 14:14:12 +0300

    333

--- src/module.js
+++ src/module.js
@@ -1,3 +1,8 @@
 
 
 console.log('module.js working')
+async function start(){
+  await Promise.resolve()
+}
+
+start();
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,75 @@
-const path = require('path')
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const fs = require("fs");
+const { clear } = require('console');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: './index.js',
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core':path.resolve(__dirname, 'src/core'),
+    }
+  },
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins:[
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: "index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.[hash].css'
+    }),
+
+
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+
+          MiniCssExtractPlugin.loader,
+
+          "css-loader",
+
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ],
+  },
 }
