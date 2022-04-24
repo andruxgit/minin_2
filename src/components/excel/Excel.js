f8eb95cddf1733a9d2f0d4d1e31e1643dc238bd2
Andru <jobandrux@yasndex.ru>
2022-04-25 01:02:17 +0300

    '3'

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,4 +3,21 @@ export class Excel {
     this.$el = document.querySelector(selector)
     this.components = options.components || []
   }
+
+  getRoot(callbackfn, thisArg) {
+    const $root = document.createElement('div');
+    this.components.forEach(Component => {
+      const component = new Component()
+      // console.log(component.toHTML())
+      $root.insertAdjacentHTML('beforeend', component.toHTML())
+    })
+    return $root;
+  }
+
+  render() {
+    // const node1 = document.createElement('h1');
+    // node1.textContent = 'TEST123'
+    this.$el.append(this.getRoot())
+    // console.log(this.$el)
+  }
 }
