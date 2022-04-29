cbcd1e9fc5be8946240ce8bc03b728c398a4066c
Andru <jobandrux@yasndex.ru>
2022-04-30 00:00:11 +0300

    3.6

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,15 +1,26 @@
+import {$} from '@core/dom'
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    // this.$el = document.querySelector(selector)
+    this.$el = $(selector)
     this.components = options.components || []
   }
 
-  getRoot(callbackfn, thisArg) {
-    const $root = document.createElement('div');
+  getRoot() {
+    // const $root = document.createElement('div');
+    // $root.classList.add('excel')
+    const $root = $.create('div', 'exel')
     this.components.forEach(Component => {
-      const component = new Component()
-      // console.log(component.toHTML())
-      $root.insertAdjacentHTML('beforeend', component.toHTML())
+      // const $el = document.createElement('div')
+      // $el.classList.add(Component.className)
+      const $el = $.create('div', Component.className)
+      const component = new Component($el)
+      // debugger
+      // $el.innerHTML = component.toHTML()
+      // $root.insertAdjacentHTML('beforeend', component.toHTML())
+      // $root.insertAdjacentHTML('beforeend', $el.innerHTML);
+      $el.html(component.toHTML());
+      $root.append($el)
     })
     return $root;
   }
@@ -17,7 +28,9 @@ export class Excel {
   render() {
     // const node1 = document.createElement('h1');
     // node1.textContent = 'TEST123'
+
     this.$el.append(this.getRoot())
+
     // console.log(this.$el)
   }
 }
